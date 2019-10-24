ead2e4e8e3f493c5dca882b2b211979bb3014239
aljeandro <alortizme@unal.edu.co>
2019-10-24 13:13:10 -0500

    Creacion de detallesVacunacionSchema y ajuste en empleadoSchema

---
+++ imports/collections/detallesVacunacionSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const detallesVacunacionSchema = new.mongoose.Schema({
+    vacuna:{ 
+    type: mongoose.Schema.Types.ObjectId,
+    require: true
+},
+/* La cantidad aplicada debe ser de tipo Number. Si es enviada 
+como un String, en type poner 'Number' incluyendo las comillas, o 
+castear el número antes de ser ingresado a la base de datos.*/
+cantidadAplicada: {
+    type: Number, 
+    default: 0  
+},
+registradoPor:{
+    type: mongoose.Schema.Types.ObjectId,
+    default: null 
+}
+})
+
+module.exports = detallesVacunacionSchema
