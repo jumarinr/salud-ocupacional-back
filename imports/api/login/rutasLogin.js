3c33b9cbb785c4c5450b8a0f56b968a3bb1cef16
Mateo Martínez <matmartinezpal@unal.edu.co>
2019-10-30 13:48:02 -0500

    Se arreglo un pequeño error de la sesion x2

--- imports/api/login/rutasLogin.js
+++ imports/api/login/rutasLogin.js
@@ -40,6 +40,7 @@ router.post('/', (req, res) => {
 router.delete('/', (req,res) =>{
     req.session.destroy(err =>{
         if (err) res.json({error: true, mensaje: "No se pudo cerrar la sesion correctamente"})
+        res.set("Session", false)
         res.json({error: false, mensaje: "Se cerro la sesion correctamente"})
     })
     
