7344ed5886e61c4de3849d87b4deddc207b1c135
Mateo Martínez <matmartinezpal@unal.edu.co>
2019-11-04 15:54:48 -0500

    Pequeñas modificaciones en cors x4

--- server/index.js
+++ server/index.js
@@ -44,9 +44,9 @@ app.use(session({
 // Middleware para comprobar que haya una session.
 // Si no la hay se agregare un false.
 app.use((req,res,next) =>{
-  if (typeof req.session.datos === 'undefined'){
+  /*if (typeof req.session.datos === 'undefined'){
     req.session.datos = false
-  }
+  }*/
   res.set("Session", JSON.stringify(req.session.datos))
   // console.log(req.session)
   // if ((req.originalUrl == "/login" && req.method == "DELETE" && !req.session.datos) || (req.originalUrl != "/login" && !req.session.datos)){
