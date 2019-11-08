2e4373831d151ba46d991a7ada5a59e52dbb6e45
Mateo Martínez <matmartinezpal@unal.edu.co>
2019-11-08 15:34:07 -0500

    Session store

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session);
+const mongoose = require("mongoose")
 
 const cors = require('cors')
 
