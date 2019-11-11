150fae9bce3912c49e2334db50b91d0272a35e9c
Mateo Martínez <matmartinezpal@unal.edu.co>
2019-11-11 14:33:52 -0500

    Volvi todo a la normalidad

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const puerto = process.env.PORT || 4000;
 
 
 /* Para usar cors */
-app.use(cors({credentials: true , exposedHeaders: ["Session","Access-Token", "Uid"]}))
+app.use(cors({credentials: true}))
 
 
 // Es necesario situar el bodyParser como un middleware para poder recibir los datos enviados por el metodo POST 
