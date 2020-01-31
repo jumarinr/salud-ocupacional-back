9ecd5f3f3aad00479cf9358052cafa13c71cf01e
Mateo Martínez <matmartinezpal@unal.edu.co>
2020-01-30 22:22:38 -0500

    Se modificaron los datos que se envian en sesion y en el body al logearse

--- imports/api/login/rutasLogin.js
+++ imports/api/login/rutasLogin.js
@@ -13,13 +13,7 @@ router.post('/', (req, res) => {
             if (usuarioEncontrado.identificacion === req.body.contrasena) {
                 req.session.datos = usuarioEncontrado
                 res.set("Session", JSON.stringify(req.session.datos))
-                res.json({error: false, mensaje: "Logeado con exito", 
-                datos: {
-                    id: usuarioEncontrado._id,
-                    identificacion : usuarioEncontrado.identificacion,
-                    nombres : usuarioEncontrado.nombres,
-                    areaTrabajo : usuarioEncontrado.areaTrabajo
-                }})
+                res.json({error: false, mensaje: "Logeado con exito", datos: usuarioEncontrado})
             } else {
                 res.json({error: true, mensaje: "Contrasena incorrecta"})
             }
