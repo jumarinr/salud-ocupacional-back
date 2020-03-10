089fe42ba60ed5d9e7ee8af4271dc5725ee385f9
Juan Diego <jumarinr@unal.edu.co>
2020-03-10 04:13:15 -0500

    prueba de la conexión a la db

--- imports/configurations/db-connection.js
+++ imports/configurations/db-connection.js
@@ -1,6 +1,9 @@
  const mongoose = require("mongoose")
 
 // Conexion a la base de datos. 
-mongoose.connect("mongodb://dev1:contrasena1@ds229068.mlab.com:29068/requi-sistemas-salud-ocupacional", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+mongoose.connect(
+    // "mongodb://dev1:contrasena1@ds229068.mlab.com:29068/requi-sistemas-salud-ocupacional", 
+    "mongodb+srv://jumarinr:Juanycarla1@score-keeper-qawt3.mongodb.net/requi-sistemas-salud-ocupacional?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 .then(db => console.log("Base de datos conectada"))
 .catch(err => console.error(err));
+
